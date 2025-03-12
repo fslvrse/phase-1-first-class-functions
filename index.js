95572e985c7ccebb813df2e4889b28bcc05cd183
faisal <fslvrse2004@gmail.com>
2025-03-12 08:31:21 +0300

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("This is a named function.");
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("This is an anonymous function.");
+  };
+}
+
+
+
+receivesAFunction(() => console.log("Callback function called!"));
+
+
+const namedFunc = returnsANamedFunction();
+namedFunc(); 
+
+const anonymousFunc = returnsAnAnonymousFunction();
+anonymousFunc();
